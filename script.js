95ec2c5eb44dadf720b9304981ca4f69cef2a36e
SabaChnaturia <154993776+SabaChnaturia@users.noreply.github.com>
2024-08-27 19:37:02 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const array = [10,20,30,40,50];
+const valueToFind = 50;
+
+if(array.includes(valueToFind)){
+    console.log("რიცხვი ნაპოვნია");
+}
+else{
+    console.log("რიცხვი არ მოიძებნა");
+}
